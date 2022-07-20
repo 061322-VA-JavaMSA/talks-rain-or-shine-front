94879a9b1248ea41cd174debc9aa43f80df1b252
EFerri222 <eferri222@gmail.com>
2022-07-20 14:14:24 -0400

    Rename angular project from talks-rain-or-shine to talks-rain-or-shine-front

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TalksRainOrShine
+# TalksRainOrShineFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'talks-rain-or-shine';
+  title = 'talks-rain-or-shine-front';
 }
